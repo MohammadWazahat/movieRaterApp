965894d5ae6edc73b25c5323b700da28c93028ef
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-07-07 19:31:21 +0530

    update form and delete functionality added

--- src/components/UpdateData.jsx
+++ src/components/UpdateData.jsx
@@ -6,8 +6,21 @@ const UpdateData = () => {
   const { id } = useParams();
   // Create api
   const users = {
-    name: " ",
-    password: " ",
+    image: "",
+    title: "",
+    description: "",
+    genre: "",
+    origin: "",
+    starting_date: "",
+    isRunning: "",
+    ending_date: "",
+    no_of_seasons: "",
+    no_of_episodes: "",
+    runtime: "",
+    date_i_watched: "",
+    my_rating: "",
+    imdb_rating: "",
+    my_review: "",
   };
 
   const [user, setUser] = useState(users);
@@ -38,46 +51,216 @@ const UpdateData = () => {
       <h2>Update your form</h2>
       <div>
         <form onSubmit={submitForm}>
-          <label htmlFor="first_name">Enter First Name</label>
-          <br />
-          <input
-            type="text"
-            placeholder="Fisrt name"
-            name="first_name"
-            value={user.first_name}
-            onChange={inputChangeHandler}
-          />
-          <br />
-          <label htmlFor="last_name">Enter Last Name</label>
-          <br />
-          <input
-            type="text"
-            placeholder="Last name"
-            name="last_name"
-            value={user.last_name}
-            onChange={inputChangeHandler}
-          />
-          <br />
-          <label htmlFor="gender">Enter gender</label>
-          <br />
-          <input
-            type="text"
-            placeholder="gender"
-            name="gender"
-            value={user.gender}
-            onChange={inputChangeHandler}
-          />
-          <br />
-          <label htmlFor="city">Enter city</label>
-          <br />
-          <input
-            type="text"
-            placeholder="city"
-            name="city"
-            value={user.city}
-            onChange={inputChangeHandler}
-          />
-          <br />
+          <div>
+            <div>
+              <label htmlFor="image">Enter image</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="image"
+                name="image"
+                value={user.image}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="title">Enter title</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="title"
+                name="title"
+                value={user.title}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="description">Enter description</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="description"
+                name="description"
+                value={user.description}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="genre">Enter genre</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="genre"
+                name="genre"
+                value={user.genre}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="origin">Enter origin</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="origin"
+                name="origin"
+                value={user.origin}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="starting_date">Enter starting_date</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="starting_date"
+                name="starting_date"
+                value={user.starting_date}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="isRunning">Enter isRunning</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="isRunning"
+                name="isRunning"
+                value={user.isRunning}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="ending_date">Enter ending_date </label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="ending_date"
+                name="ending_date"
+                value={user.ending_date}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="no_of_seasons">Enter no_of_seasons</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="no_of_seasons"
+                name="no_of_seasons"
+                value={user.no_of_seasons}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="no_of_episodes">Enter no_of_episodes</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="no_of_episodes"
+                name="no_of_episodes"
+                value={user.no_of_episodes}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="runtime">Enter runtime</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="runtime"
+                name="runtime"
+                value={user.runtime}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="date_i_watched">Enter date_i_watched</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="date_i_watched"
+                name="date_i_watched"
+                value={user.date_i_watched}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="my_rating">Enter my_rating</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="my_rating"
+                name="my_rating"
+                value={user.my_rating}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="imdb_rating">Enter imdb_rating</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="imdb_rating"
+                name="imdb_rating"
+                value={user.imdb_rating}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="my_review">Enter my_review</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="my_review"
+                name="my_review"
+                value={user.my_review}
+                onChange={inputChangeHandler}
+              />
+            </div>
+          </div>
           <button type="submit">Submit</button>
         </form>
       </div>
